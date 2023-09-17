519a839954a26a0558e66aeb13a3d3255fe10f96
Noah Wagner <wagnernoah@icloud.com>
2023-09-17 16:04:23 -0700

    added access() check and readdir error check

--- Assignment 3/assignment3.c
+++ Assignment 3/assignment3.c
@@ -6,90 +6,8 @@
 #include <dirent.h>
 #include <stdio.h>
 
-/* Description:
- * 	Recursively finds every regular readable file under inputPath
- *
- * Params:
- *	inputPath: the path to start counting regular readable files
- *
- * Return:
- * 	on success, return total number of regular readable files encountered
- *	on failure, print errno message stderror and return the negative of
- *	errno
- */
-int find_readables(char *inputPath)
-{
-	struct stat area;
-	struct stat *s = &area;
-
-	// base case 1: something went wrong trying to lstat file or directory
-	if (lstat(inputPath, s) == -1) {
-		// no read permission sub-case
-		if (errno == EACCES)
-			return 0;
-		fprintf(stderr, "%s\n", strerror(errno));
-		return -errno;
-	}
-
-	// base case 2: reached a readable regular file
-	if (S_ISREG(s->st_mode) && (s->st_mode & S_IRUSR)) {
-		return 1;
-	} else if (!S_ISDIR(s->st_mode)) {
-		/*
-		 * base case 3: reached special file (including symbolic links)
-		 * or non-readable regular file
-		 */
-		return 0;
-	}
-
-	DIR *dir = opendir(inputPath);
-
-	// base case 4: failed to open the directory
-	if (dir == NULL) {
-		// no read permission sub-case
-		if (errno == EACCES)
-			return 0;
-		fprintf(stderr, "%s\n", strerror(errno));
-		return -errno;
-	}
-
-	// base case 5: failed to change directory to dir
-	if (chdir(inputPath) == -1) {
-		int tmp_error = errno;
-		closedir(dir);
-		// no execute permission sub-case
-		if (tmp_error == EACCES)
-			return 0;
-		fprintf(stderr, "%s\n", strerror(tmp_error));
-		return -tmp_error;
-	}
-
-	struct dirent *entry;
-
-	int readables = 0;
-
-	// recursion: if we reached a searchable directory...
-	while ((entry = readdir(dir)) != NULL) {
-		// don't step into . or .., this leads to infinite loops
-		if (!strcmp(entry->d_name, ".") || 
-		    !strcmp(entry->d_name, ".."))
-			continue;
-
-		int tmp = find_readables(entry->d_name);
-
-		if (tmp < 0) {
-			// error occured, return the error code
-			readables = tmp;
-			break;
-		} else {
-			readables += tmp;
-		}
-	}
-
-	chdir("..");
-	closedir(dir);
-	return readables;
-}
+static int find_readables(char *inputPath);
+int readable(char *inputPath);
 
 /* Description:
  * 	wrapper function for find_readables, checks for initial base cases
@@ -140,4 +58,112 @@ int readable(char *inputPath)
 	}
 
 	return find_readables(inputPath);
+}
+
+/* Description:
+ * 	Recursively finds every regular readable file under inputPath
+ *
+ * Params:
+ *	inputPath: the path to start counting regular readable files
+ *
+ * Return:
+ * 	on success, return total number of regular readable files encountered
+ *	on failure, print errno message stderror and return the negative of
+ *	errno
+ */
+static int find_readables(char *inputPath)
+{
+	struct stat area;
+	struct stat *s = &area;
+
+	// base case 1: something went wrong trying to lstat file or directory
+	if (lstat(inputPath, s) == -1) {
+		// no read permission sub-case
+		if (errno == EACCES)
+			return 0;
+		fprintf(stderr, "%s\n", strerror(errno));
+		return -errno;
+	}
+
+	// base case 2: reached a regular file
+	if (S_ISREG(s->st_mode)) {
+		if(access(inputPath, R_OK) < 0) {
+			if(errno == EACCES)
+				return 0;
+			fprintf(stderr, "%s\n", strerror(errno));
+			return -errno;
+		} else {
+			return 1;
+		}
+
+	} else if (!S_ISDIR(s->st_mode)) {
+		/*
+		 * base case 3: reached special file (including symbolic links)
+		 * or non-readable regular file
+		 */
+		return 0;
+	}
+
+	DIR *dir = opendir(inputPath);
+
+	// base case 4: failed to open the directory
+	if (dir == NULL) {
+		// no read permission sub-case
+		if (errno == EACCES)
+			return 0;
+		fprintf(stderr, "%s\n", strerror(errno));
+		return -errno;
+	}
+
+	// base case 5: failed to change directory to dir
+	if (chdir(inputPath) == -1) {
+		int tmp_error = errno;
+		closedir(dir);
+		// no execute permission sub-case
+		if (tmp_error == EACCES)
+			return 0;
+		fprintf(stderr, "%s\n", strerror(tmp_error));
+		return -tmp_error;
+	}
+
+	struct dirent *entry;
+
+	int readables = 0;
+
+	// done to error check readdir()
+	errno = 0;
+
+	// recursion: if we reached a searchable directory...
+	while ((entry = readdir(dir)) != NULL) {
+		// don't step into . or .., this leads to infinite loops
+		if (!strcmp(entry->d_name, ".") || 
+		    !strcmp(entry->d_name, ".."))
+			continue;
+
+		int tmp = find_readables(entry->d_name);
+
+		if (tmp < 0) {
+			// error occured, return the error code
+			readables = tmp;
+			break;
+		} else {
+			readables += tmp;
+
+			/* errno could still be equal to EACCES
+			 * reset it for readdir error check since we
+			 * don't care about permission denied errors
+			 */
+			errno = 0;
+		}
+	}
+
+	// check if readdir failed
+	if(readables > 0 && errno != 0) {
+		readables = -errno;
+		fprintf(stderr, "%s\n", strerror(errno));
+	}
+
+	chdir("..");
+	closedir(dir);
+	return readables;
 }
